0bde0fe7f58a96b409f805839bdce0dae98d26e0
Shine Lin Ko <40501827@napier.ac.uk>
2022-03-22 13:28:13 +0630

    Create City.java class

---
+++ src/main/java/com/napier/devops/City.java
@@ -0,0 +1,23 @@
+package com.napier.devops;
+
+/**
+ * Represents City
+ */
+public class City {
+    /**
+     * Represent name of city
+     */
+    public String name;
+    /**
+     * Represent country code of city
+     */
+    public String countryCode;
+    /**
+     * Represent name of district
+     */
+    public String district;
+    /**
+     * Represent population of city
+     */
+    public int population;
+}
