5206d9b30317896e243b9f6e83ad8f44e995d7c3
Shine Lin Ko <40501827@napier.ac.uk>
2022-03-22 13:49:32 +0630

    Add getTopCountryWorld to get top 10 populated countries in world Add printTopCountryWorld to show top 10 populated countries in world

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -18,6 +18,11 @@ public class App
         //Print list of Countries by largest population to smallest
         a.printCountryList(countrylist);
 
+        ArrayList<Country> topcountrylistworld = a.getTopCountryWorld();
+        System.out.println(" ---------- Top 10 countries organized by largest to smallest population ---------- ");
+        //Print list of Countries by largest population to smallest
+        a.printTopCountryWorld(topcountrylistworld);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -140,6 +145,53 @@ public class App
 
     //Print list of Countries by largest population to smallest
     public void printCountryList(ArrayList<Country> countrylist) {
+        // Print header
+        System.out.println(String.format("%-10s %-25s %-25s %-35s %-25s %10s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        // Loop over all countries in the list
+        for (Country country : countrylist) {
+            if (country == null)
+                continue;
+            String cty_string =
+                    String.format("%-10s %-25s %-25s %-35s %-25s %10s",
+                            country.code, country.name, country.continent, country.region, country.population, country.capital);
+            System.out.println(cty_string);
+        }
+    }
+
+    //Get top 10 countries in the world organised by largest population to smallest.
+    public ArrayList<Country> getTopCountryWorld() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, Population, Capital "
+                            + "FROM country "
+                            + "ORDER BY Population DESC LIMIT 10";
+            // Execute SQL statement
+            ResultSet rest = stmt.executeQuery(strSelect);
+            // Extract Country information
+            ArrayList<Country> countrylist = new ArrayList<Country>();
+            while (rest.next()) {
+                Country country = new Country();
+                country.code = rest.getString("Code");
+                country.name = rest.getString("Name");
+                country.continent = rest.getString("Continent");
+                country.region = rest.getString("Region");
+                country.population = rest.getInt("Population");
+                country.capital = rest.getInt("Capital");
+                countrylist.add(country);
+            }
+            return countrylist;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Countries by largest population to smallest");
+            return null;
+        }
+    }
+
+    //Print top 10 countries in the world Countries by largest population to smallest
+    public void printTopCountryWorld(ArrayList<Country> countrylist) {
         // Print header
         System.out.println(String.format("%-10s %-25s %-25s %-35s %-25s %10s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all countries in the list
