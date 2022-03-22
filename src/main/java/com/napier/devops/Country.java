6642b4b78c05639c4590648a0eb19c54a71b5531
Kaung Min Htet <40501822@live.napier.ac.uk>
2022-03-22 11:13:57 +0630

    Create Country file

---
+++ src/main/java/com/napier/devops/Country.java
@@ -0,0 +1,34 @@
+package com.napier.devops;
+
+public class Country {
+    /**
+     * Country Code
+     */
+    public int code;
+
+    /**
+     * Country Name
+     */
+    public String name;
+
+    /**
+     * Country Continent
+     */
+    public String continent;
+
+    /**
+     * Country Region
+     */
+    public String region;
+
+    /**
+     * Country Population
+     */
+    public int population;
+
+    /**
+     * Country Capital
+     */
+    public String capital;
+
+}
