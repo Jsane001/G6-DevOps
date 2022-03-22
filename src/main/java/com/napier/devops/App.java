09fd13cd314e7dda11d555d9aa57d9256303cb46
Ar Kar Min <101619972+40478648@users.noreply.github.com>
2022-03-22 23:29:09 +0630

    Show & Get largest to smallest countries region

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -153,4 +153,51 @@ public class App
         }
     }
 
+    //Countries in the world organised by largest population to smallest in region.
+    public ArrayList<Country> getCountryRegion() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, Population, Capital "
+                            + "FROM country WHERE Region = 'Southern and Central Asia' "
+                            + "ORDER BY Population DESC";
+            // Execute SQL statement
+            ResultSet rest = stmt.executeQuery(strSelect);
+            // Extract Country information
+            ArrayList<Country> countrylist = new ArrayList<Country>();
+            while (rest.next()) {
+                Country country = new Country();
+                country.code = rest.getString("Code");
+                country.name = rest.getString("Name");
+                country.continent = rest.getString("Continent");
+                country.region = rest.getString("Region");
+                country.population = rest.getInt("Population");
+                country.capital = rest.getInt("Capital");
+                countrylist.add(country);
+            }
+            return countrylist;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Countries by largest population to smallest");
+            return null;
+        }
+    }
+
+    //Print list of Countries by largest population to smallest
+    public void printCountryRegion(ArrayList<Country> countrylist) {
+        // Print header
+        System.out.println(String.format("%-10s %-25s %-25s %-35s %-25s %10s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        // Loop over all countries in the list
+        for (Country country : countrylist) {
+            if (country == null)
+                continue;
+            String cty_string =
+                    String.format("%-10s %-25s %-25s %-35s %-25s %10s",
+                            country.code, country.name, country.continent, country.region, country.population, country.capital);
+            System.out.println(cty_string);
+        }
+    }
+
 }
