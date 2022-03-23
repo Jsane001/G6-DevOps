0dcde4711b00dd105434c44bfcf30753070d6511
Kaung Min Htet <40501822@live.napier.ac.uk>
2022-03-23 14:41:29 +0630

    Get and show top ten populated countries in continent.(Asia)

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -23,6 +23,11 @@ public class App
         //Print Top 10 Populated Country List in Region
         a.printTopTenCountryList(topTenCountryList);
 
+        ArrayList<Country> topTenCountryInContinent = a.getTopTenCountryInContinent();
+        System.out.println(" ---------- Top 10 Populated Country List in Continent ---------- ");
+        //Print Top 10 Populated Country List in Continent
+        a.printTopTenCountryList(topTenCountryInContinent);
+
         // Disconnect from database
         a.disconnect();
     }
@@ -199,6 +204,56 @@ public class App
         System.out.println(String.format("%-10s %-25s %-25s %-35s %-25s %10s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
         // Loop over all countries in the list
         for (Country country : toptencountrylist) {
+            if (country == null)
+                continue;
+            String cty_string =
+                    String.format("%-10s %-25s %-25s %-35s %-25s %10s",
+                            country.code, country.name, country.continent, country.region, country.population, country.capital);
+            System.out.println(cty_string);
+        }
+    }
+
+    //Top 10 populated countries in continent list by largest to smallest population.
+    public ArrayList<Country> getTopTenCountryInContinent() {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT Code, Name, Continent, Region, Population, Capital "
+                            + "FROM country "
+                            + "WHERE Continent = 'Asia' "
+                            + "ORDER BY Population DESC "
+                            + "LIMIT 10";
+            // Execute SQL statement
+            ResultSet rest = stmt.executeQuery(strSelect);
+            // Extract Country information
+            ArrayList<Country> toptencountryincontinent = new ArrayList<Country>();
+            while (rest.next()) {
+                Country country = new Country();
+                country.code = rest.getString("Code");
+                country.name = rest.getString("Name");
+                country.continent = rest.getString("Continent");
+                country.region = rest.getString("Region");
+                country.population = rest.getInt("Population");
+                country.capital = rest.getInt("Capital");
+                toptencountryincontinent.add(country);
+            }
+            return toptencountryincontinent;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get top 10 populated countries in continent by largest population to smallest");
+            return null;
+        }
+    }
+
+    //Print list of top 10 populated countries in continent by largest to smallest population
+    public void printTopTenCountryInContinent(ArrayList<Country> toptencountryincontinent) {
+        // Print header
+        System.out.println(" ---------- Top 10 Populated Countries in Continent List by largest to smallest population ---------- ");
+        System.out.println(String.format("%-10s %-25s %-25s %-35s %-25s %10s", "Code", "Name", "Continent", "Region", "Population", "Capital"));
+        // Loop over all countries in the list
+        for (Country country : toptencountryincontinent) {
             if (country == null)
                 continue;
             String cty_string =
