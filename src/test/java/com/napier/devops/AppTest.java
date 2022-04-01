d42e01e42baf6d41ad4ee3915078b41e21f1070f
Shine Lin Ko <40501827@napier.ac.uk>
2022-04-01 09:11:06 +0630

    Add initial testing file

---
+++ src/test/java/com/napier/devops/AppTest.java
@@ -0,0 +1,11 @@
+package com.napier.devops;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+}
