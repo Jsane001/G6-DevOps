d9cb2d0d4b162cf5202194c7bcde35b6b3b0afb2
NyeinChanSoe2 <40501807@live.napier.ac.uk>
2022-04-01 10:56:14 +0630

    Added testing method for printTopCapitalWorld function

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1242,7 +1242,12 @@ public class App
      * Print top 10 capital city in world by largest population to smallest
      */
     public void printTopCapitalWorld(ArrayList<Capital_City> capitalList) {
-
+        // Check capitalList is not null
+        if (capitalList == null)
+        {
+            System.out.println("No Population capital");
+            return;
+        }
         // Print header
         System.out.printf("%-30s %-25s %-10s%n", "Capital", "Name", "Population");
         // Loop over all countries in the list
