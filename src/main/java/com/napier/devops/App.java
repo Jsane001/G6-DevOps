12bf5eb8122a6bc886eda2737c81773e7dd6b461
NyeinChanSoe2 <40501807@live.napier.ac.uk>
2022-04-01 11:06:04 +0630

    Added testing method for printTopCityWorld function

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -838,6 +838,12 @@ public class App
      * Show top 10 city in the world organised by largest population to smallest
      */
     public void printTopCityWorld(ArrayList<City> cityList) {
+        // Check cityList is not null
+        if (cityList == null)
+        {
+            System.out.println("No Population city");
+            return;
+        }
         // Print header
         System.out.printf("%-30s %-10s %-20s %10s%n", "Name", "Country", "District", "Population");
         // Loop over all city in the list
