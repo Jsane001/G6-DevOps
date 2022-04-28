fb0fa17576e8ffc4e49142b64a707c53025de47e
Ar Kar Min <40478648@live.napier.ac.uk>
2022-04-28 23:08:16 +0630

    Add title for CityContinent

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -759,6 +759,7 @@ public class App
         }
         // Print header
         StringBuilder sb = new StringBuilder();
+        sb.append("# 9.  List of cities in the continent organised by largest population to smallest\r\n");
         sb.append("| Name | Country | District | Population |\r\n");
         sb.append("| --- | --- | --- | --- |\r\n");
         // Loop over all city in the list
